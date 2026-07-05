43296bd159649d192fa49befa5620360305307b7
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2026-07-06 05:53:40 +0700

    Create constants.js

---
+++ src/shared/constants.js
@@ -0,0 +1,13 @@
+module.exports = {
+  CURRENCY: ["USD", "EUR", "IDR"],
+  TX_STATUS: {
+    PENDING: "PENDING",
+    POSTED: "POSTED",
+    FAILED: "FAILED"
+  },
+  ISO_MESSAGES: {
+    PACS_008: "pacs.008.001.08",
+    PACS_002: "pacs.002.001.10",
+    CAMT_053: "camt.053.001.08"
+  }
+};

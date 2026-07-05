24c385b381799c0f23c484d1305befe24a7b9cfe
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2026-07-06 05:59:35 +0700

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -3,14 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+const transactionRoutes = require("./routes/transaction.routes");
+
+app.use("/api/transactions", transactionRoutes);
+
 app.get("/health", (req, res) => {
-  res.json({
-    status: "OK",
-    system: "KONG BANK CORE",
-    timestamp: new Date()
-  });
+  res.json({ status: "OK", system: "KONG BANK CORE" });
 });
 
 app.listen(4000, () => {
-  console.log("🏦 KONG BANK API RUNNING ON PORT 4000");
+  console.log("🏦 KONG BANK RUNNING ON PORT 4000");
 });
